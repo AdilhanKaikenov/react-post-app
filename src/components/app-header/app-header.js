b340b50773c91e14121c8b90ea5561510bc2ab5a
Kaikenov Adilhan <adilhan_kai@mail.ru>
2020-11-25 20:08:25 +0600

    added styled components

--- src/components/app-header/app-header.js
+++ src/components/app-header/app-header.js
@@ -1,13 +1,31 @@
 import React from 'react';
 
 import './app-header.css';
+import styled from 'styled-components';
+
+const Header = styled.div`
+    display: flex;
+    align-items: flex-end;
+    justify-content: space-between;
+    h1 {
+        font-size: 26px;
+        color: ${props => props.colored ? 'blue' : 'black'};
+        :hover {
+            color: black;
+        }
+    }
+    h2 {
+        font-size: 1.2rem;
+        color: grey;
+    }
+`
 
 const AppHeader = () => {
     return (
-        <div className="app-header d-flex">
+        <Header colored>
             <h1>Kaikenov Adilkhan</h1>
             <h2>3 записей, из них понравилось 0</h2>
-        </div>
+        </Header>
     )
 }
 
