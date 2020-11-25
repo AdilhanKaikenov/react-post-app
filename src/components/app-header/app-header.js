26e143e91ac2a788751fda7eb9daafde81a42579
Kaikenov Adilhan <adilhan_kai@mail.ru>
2020-11-25 12:17:46 +0600

    added some components

---
+++ src/components/app-header/app-header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AppHeader = () => {
+    return (
+        <div className="post-header d-flex">
+            <h1>Kaikenov Adilkhan</h1>
+            <h2>5 записей, из них понравилось 0</h2>
+        </div>
+    )
+}
+
+export default AppHeader;
