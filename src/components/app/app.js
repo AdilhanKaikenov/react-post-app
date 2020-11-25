616470ac25ca73d22c9b152d8c103f74493c4422
Kaikenov Adilhan <adilhan_kai@mail.ru>
2020-11-25 12:37:23 +0600

    added post list component

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,10 +1,21 @@
 import React from 'react';
 
 import AppHeader from '../app-header';
+import SearchPanel from '../search-panel';
+import PostStatusFilter from '../post-status-filter';
+import PostList from '../post-list';
+
 
 const App = () => {
     return (
-        <AppHeader/>
+        <div className="app">
+            <AppHeader/>
+            <div className="search-panel d-flex">
+                <SearchPanel/>
+                <PostStatusFilter/>
+            </div>
+            <PostList/>
+        </div>
     );
 }
 
