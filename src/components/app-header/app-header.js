3936d9edc60f19405ad56fe6d99ca10e76d1ddbd
Kaikenov Adilhan <adilhan_kai@mail.ru>
2020-11-25 15:34:21 +0600

    css files added

--- src/components/app-header/app-header.js
+++ src/components/app-header/app-header.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
+import './app-header.css';
+
 const AppHeader = () => {
     return (
         <div className="post-header d-flex">
             <h1>Kaikenov Adilkhan</h1>
-            <h2>5 записей, из них понравилось 0</h2>
+            <h2>3 записей, из них понравилось 0</h2>
         </div>
     )
 }
